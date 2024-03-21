102ec5ff485be2014aabe2918f44791c59ece462
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-03-21 10:29:55 -0700

    3

---
+++ home work/day.28/homework/3/script.js
@@ -0,0 +1,8 @@
+let num1 = 61
+let num2 = 52
+console.log(num1 > num2)
+console.log(num1 >= num2)
+console.log(num1 < num2)
+console.log(num1 <= num2)
+console.log(num1 == num2)
+console.log(num1 != num2)
