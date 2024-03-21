653cd69c4f2e243074f9ac4e5d965debd1c31923
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-03-21 10:36:24 -0700

    6

---
+++ home work/day.28/homework/6/script.js
@@ -0,0 +1,8 @@
+const num1 = 25621
+const num2 = 6843
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+console.log(parseInt(num1 / num2))
