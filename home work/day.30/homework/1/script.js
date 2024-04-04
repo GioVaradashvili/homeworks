796b65579e9418eae61c75063080a70e987f109c
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-04-04 04:41:00 -0700

    1

---
+++ home work/day.30/homework/1/script.js
@@ -0,0 +1,20 @@
+const myImfo = {
+    name: "gio",
+    lastname: "varadashvili",
+    age: 15,
+    adress: "kvareli",
+}
+
+console.log(myImfo)
+
+console.log(myImfo.name);
+console.log(myImfo.lastname);
+console.log(myImfo.age);
+console.log(myImfo.adress);
+
+myImfo.name = "giorgi";
+myImfo.lastname = "varadashvili";
+myImfo.age = 15;
+myImfo.adress = "kvareli-gavazi";
+
+console.log(myImfo)
