93b9c9cbd53414314a3f15eac267a412d962c038
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-04-04 04:24:27 -0700

    .

---
+++ home work/day.30/classwork/script.js
@@ -0,0 +1,9 @@
+const myInfo = {
+    name: "lasha",
+    lastname: "varadashvili",
+    age: 15 ,
+    school: "gavazi hight school",
+    academy: "goal-oriented-academy",
+}
+
+myInfo.name="giorgi"
