d8dd639391f3c80d4a3dce2e563097203a47a445
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-04-04 09:29:23 -0700

    2

---
+++ home work/day.30/homework/2/script.js
@@ -0,0 +1,14 @@
+const mom = {
+    name: "qeti",
+    lastname: "varadashvili",
+    age: "42",
+}
+
+const grandmother = {
+    name: "zina",
+    lastname: "varadashvili",
+    age: 60,
+}
+
+console.log(mom);
+console.log(grandmother);
