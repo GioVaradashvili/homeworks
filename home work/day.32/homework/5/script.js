3379952fd6d3fff2af5919189e56bc1dd2eec193
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-04-12 10:11:03 -0700

    5

---
+++ home work/day.32/homework/5/script.js
@@ -0,0 +1,17 @@
+
+const best_film_of_mine = {
+    name: "gladiator",
+    genre: "history",
+    release_year: 2000
+ }
+ 
+ 
+ const best_film_of_mine1 = {
+     name: "The Godfather",
+     genre: "mafia",
+     release_year: 1972
+ }
+ 
+ console.log(best_film_of_mine);
+ console.log(best_film_of_mine1);
+ 
