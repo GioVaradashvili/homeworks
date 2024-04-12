6ace977446a7f2baebfe6e08cec67436f296f0dc
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-04-12 09:40:51 -0700

    2

---
+++ home work/day.32/homework/2/script.js
@@ -0,0 +1,12 @@
+function minus(){
+    const p = document.getElementById("p1")
+    p1.textContent = 3
+}
+
+function plus(){
+    p1.textContent = 5
+}
+
+function reset(){
+    p1.textContent = 4
+}
