fb122b4969013f64d60ed9d6d3971383bfe7c79c
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-04-12 09:49:27 -0700

    3

---
+++ home work/day.32/homework/3/script.js
@@ -0,0 +1,30 @@
+
+function green(){
+    const p1 = document.getElementById("p1")
+    const p2 = document.getElementById("p2")
+    const p3 = document.getElementById("p3")
+ 
+    p1.style.color = "green";
+    p2.style.color = "green";
+    p3.style.color = "green";
+}
+
+function blue(){
+    const p1 = document.getElementById("p1")
+    const p2 = document.getElementById("p2")
+    const p3 = document.getElementById("p3")
+ 
+    p1.style.color = "blue";
+    p2.style.color = "blue";
+    p3.style.color = "blue";
+}
+
+function orange(){
+    const p1 = document.getElementById("p1")
+    const p2 = document.getElementById("p2")
+    const p3 = document.getElementById("p3")
+ 
+    p1.style.color = "orange";
+    p2.style.color = "orange";
+    p3.style.color = "orange";
+}
